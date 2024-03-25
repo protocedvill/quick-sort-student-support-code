712bac34891049a5f139113109049e1a8f008272
louis <louison.savarese@gmail.com>
2024-03-25 13:45:12 -0400

    Commented & Implemented

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class QuickSort {
 
     /**
@@ -9,25 +11,79 @@ public class QuickSort {
     public static <E extends Comparable<? super E>>
     void quicksort(Iterator<E> begin, Iterator<E> end) {
         if (begin.equals(end)) return;
-
-        Iterator<E> traverse = begin;
-        Iterator<E> pivot = begin.clone();
-        Iterator<E> low = begin.clone();
-        Iterator<E> high = begin.clone();
-        while (!traverse.equals(end)) {
-            if(0 < traverse.get().compareTo(pivot.get())) {
-                // if holding is greater than the pivot then
-                low.advance();
-                Algorithms.iter_swap(high, low);
-                traverse.advance();
-            } else {
-                low.advance();
-                high.advance();
-                traverse.advance();
+        Iterator<E> b = begin.clone();
+        Iterator<E> e = end.clone();
+        Iterator<E> pivot = partition(b,e);
+        if (length(begin.clone(), end.clone()) > 3) {
+            quicksort(begin.clone(), pivot.clone());
+            if (!pivot.equals(end)) {
+                pivot.advance();
+                quicksort(pivot.clone(), end.clone());
             }
         }
-        low.advance();
-        //Algorithms.iter_swap(low,pivot);
+    }
+
+    private static <E extends Comparable<? super E>>
+    Iterator<E> min(Iterator<E> begin, Iterator<E> end) {
+        Iterator<E> m = begin;
+        for (; ! begin.equals(end); begin.advance()) {
+            Iterator<E> c = begin;
+            if (m.get().compareTo(c.get()) < 0) {
+                m = c;
+            }
+        }
+        return m;
+    }
+
+    private static <E extends Comparable<? super E>>
+    Iterator<E> random(Iterator<E> begin, Iterator<E> end) {
+        Random rand = new Random();
+        Iterator<E> m = begin;
+        int len = length(begin.clone(), end.clone());
+        int cap = rand.nextInt(len);
+        int x = 0;
+        for (Iterator<E> j = begin.clone();; j.advance()) {
+            if (x == cap) break;
+            x++;
+            m = j;
+        }
+        return m;
+    }
+
+    protected static <E extends Comparable<? super E>>
+    int length(Iterator<E> begin, Iterator<E> end) {
+        int i = 1;
+        for (Iterator<E> j = begin.clone();; j.advance()) {
+            if (j.equals(end)) break;
+            i++;
+        }
+        return i;
+    }
+
+    public static <E extends Comparable<? super E>>
+    Iterator<E> partition(Iterator<E> begin, Iterator<E> end) {
+        if (begin.equals(end)) return begin;
+
+        int length = length(begin.clone(),end.clone());
+        Iterator<E> pivot = random(begin.clone(),end.clone());
+        System.out.println(pivot);
+        Algorithms.iter_swap(pivot,end);
+        pivot = end.clone();
+        Iterator<E> i = begin.clone();
+        for (Iterator<E> j = begin.clone();;) {
+            if (0 > j.get().compareTo(pivot.get())) {
+                System.out.println("swapping " + i + " & " + j);
+                Algorithms.iter_swap(i, j);
+                i.advance();
+            }
+            if (0 == j.get().compareTo(pivot.get())) {
+                System.out.println("equals");
+            }
+            if(j.equals(end)) break;
+            j.advance();
+        }
+        Algorithms.iter_swap(i, end);
+        return i;
     }
 
 }
--- test/StudentTest.java
+++ test/StudentTest.java
@@ -12,16 +12,31 @@ public class StudentTest {
     @Test
     public void quicksort() {
         Random r = new Random();
-        Integer[] a = new Integer[10];
-        for (int i = 0; i<10; i++) {
+        Integer[] a = new Integer[1000];
+        for (int i = 0; i<1000; i++) {
             a[i] = r.nextInt(10);
         }
-        ArraySequence<Integer> array = new ArraySequence<Integer>(a);
+        ArraySequence<Integer> array = new ArraySequence<>(a);
         System.out.println(array);
         QuickSort.quicksort(array.begin(),Algorithms.last(array.begin(),array.end()));
         System.out.println(array);
     }
 
+    @Test
+    public void partition() {
+        Integer[] a = {2,3};
+        ArraySequence<Integer> array = new ArraySequence<>(a);
+        System.out.println(array);
+        QuickSort.partition(array.begin(),Algorithms.last(array.begin(),array.end()));
+        System.out.println(array);
+    }
+
+    @Test
+    public void length() {
+        Integer[] a = {1,2,5,3,3,4,5};
+        ArraySequence<Integer> array = new ArraySequence<>(a);
+        System.out.println(QuickSort.length(array.begin(),Algorithms.last(array.begin(),array.end())));
+    }
     @Test
     public void test() {
         quicksort();
